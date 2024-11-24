69668fbd0e5266015507a36a38735d4c2b839318
hardik979 <pateriyag999@gmail.com>
2024-11-24 18:31:23 +0530

    little change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import Navbar from "./components/navbar";
 import { Routes, Route, Navigate } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import LogInPage from "./pages/LogInPage";
-import SettingsPage from "./pages/SettingsPage";
-import SignUpPage from "./pages/Signuppage";
-import ProfilePage from "./pages/ProfilePage";
+import HomePage from "./pages/HomePage.jsx";
+import LogInPage from "./pages/LogInPage.jsx";
+import SettingsPage from "./pages/SettingsPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
 import { useAuthStore } from "./store/useAuthStore";
 import { useThemeStore } from "./store/useThemeStore";
 import { Toaster } from "react-hot-toast";
